e92e7b64b2735e8b16827e8fc267a9b9a6e0a349
Sokol1k <danil.sokolowskiy@gmail.com>
2020-02-20 18:12:35 +0200

    Created seed for table products

---
+++ database/seeds/products.js
@@ -0,0 +1,20 @@
+var faker = require("faker");
+
+let createRecord = knex => {
+  return knex("products").insert({
+    name: faker.lorem.words(),
+    price: faker.random.number()
+  });
+};
+
+exports.seed = function(knex) {
+  return knex("products")
+    .del()
+    .then(function() {
+      let records = [];
+      for (let i = 1; i < 50; i++) {
+        records.push(createRecord(knex));
+      }
+      return Promise.all(records);
+    });
+};
