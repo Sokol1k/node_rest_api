d04cc1771ede488d63553589a57af00c518ce01d
Sokol1k <danil.sokolowskiy@gmail.com>
2020-02-20 17:34:30 +0200

    Created seed for table users

---
+++ database/seeds/users.js
@@ -0,0 +1,11 @@
+exports.seed = function(knex) {
+  return knex("users")
+    .del()
+    .then(function() {
+      return knex("users").insert([
+        { name: "Vasya", email: "shop-assistant@gamil.com", role_id: 1 },
+        { name: "Petya", email: "cashier@gamil.com", role_id: 2 },
+        { name: "Vova", email: "accountant@gamil.com", role_id: 3 },
+      ]);
+    });
+};
