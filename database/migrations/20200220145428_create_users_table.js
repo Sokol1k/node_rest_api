890be7726fdff7499a206e0b8cececcd14ee0401
Sokol1k <danil.sokolowskiy@gmail.com>
2020-02-20 15:37:47 +0200

    Updated migration create user table

--- database/migrations/20200220145428_create_users_table.js
+++ database/migrations/20200220145428_create_users_table.js
@@ -3,7 +3,13 @@ exports.up = function(knex) {
     table.increments();
     table.string("email", 255).notNullable();
     table.string("name", 255).notNullable();
-    table.integer("role").unsigned().notNullable().references('roles.id');
+    table
+      .integer("role_id")
+      .unsigned()
+      .notNullable()
+      .references("roles.id")
+      .onDelete()
+      .onUpdate();
     table.timestamps();
     table.unique("email");
   });
