da7c95579836dd0e927debd70f6e04f2505fda7f
Sokol1k <danil.sokolowskiy@gmail.com>
2020-02-21 19:10:03 +0200

    Added user permission check and validation for order.store

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const checkAuth = require('./check-auth');
-const auth = require('./authorization');
+const checkAuth = require('./authorization/check-auth');
+const auth = require('./authorization/authorization');
+const orderStore = require('./order/store');
+
 
 router.use(checkAuth.middleware);
 router.post('/login', auth.rules, auth.middleware);
+router.post('/order', orderStore.rules, orderStore.middleware);
 
 module.exports = router;
---
+++ middleware/order/store.js
@@ -0,0 +1,25 @@
+const { check, validationResult } = require("express-validator");
+
+module.exports = {
+  rules: [
+    check("product_id", "Product id is not valid")
+      .not()
+      .isEmpty()
+      .isNumeric()
+  ],
+  middleware: function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).send(errors.array());
+    } else {
+      var user = req.cookies["user"];
+      if (user.role_id == 2) {
+        next();
+      } else {
+        return res.status(401).send({
+          message: "User does not have permission to use this method."
+        });
+      }
+    }
+  }
+};
