d4c2f07719772980c287473469ffddf20392c9c5
Sokol1k <danil.sokolowskiy@gmail.com>
2020-02-23 19:21:00 +0200

    Added a unit test for the check.show method

--- test/check.test.js
+++ test/check.test.js
@@ -29,11 +29,35 @@ it("check.store: User not authorized", function(done) {
     .end(done);
 });
 
-it("check.store: User not authorized", function(done) {
+it("check.store: Invalid data", function(done) {
   request(app)
     .post("/api/checks")
     .send({ order_id: "A" })
     .set("Cookie", ["user=2", "role=2"])
     .expect(422)
+    .end(done);
+});
+
+it("check.show: Get check", function(done) {
+  request(app)
+    .get("/api/checks/1")
+    .set("Cookie", ["user=2", "role=2"])
+    .expect(200)
+    .end(done);
+});
+
+it("check.show: User does not have permission to use this method", function(done) {
+  request(app)
+    .get("/api/checks/1")
+    .set("Cookie", ["user=2", "role=1"])
+    .expect(401)
+    .end(done);
+});
+
+it("check.show: User not authorized", function(done) {
+  request(app)
+    .get("/api/checks/1")
+    .set("Cookie", ["role=2"])
+    .expect(401)
     .end(done);
 });
