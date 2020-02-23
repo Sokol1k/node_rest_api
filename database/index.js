918581947ef2ff143c00839bb2ab18561888047d
Sokol1k <danil.sokolowskiy@gmail.com>
2020-02-23 20:47:27 +0200

    Added a .env file

--- database/index.js
+++ database/index.js
@@ -1,3 +1,5 @@
-let environment = process.env.NODE_ENV || 'development';
+require('dotenv').config()
+
+let environment = process.env.NODE_ENV;
 const config = require('../knexfile')[environment];
 module.exports = require('knex')(config);
