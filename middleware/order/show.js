7c7d230fcbc80289bf6c39a7bae054b51a3bf372
Sokol1k <danil.sokolowskiy@gmail.com>
2020-02-24 23:38:49 +0200

    Added middleware to check user permissions for the order.show method

---
+++ middleware/order/show.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+  middleware: function(req, res, next) {
+    let role = jwt.decode(req.token).user.role_id;
+    if (role == 1 || role == 3) {
+      next();
+    } else {
+      return res.status(403).send({
+        message: "User does not have permission to use this method."
+      });
+    }
+  }
+};
